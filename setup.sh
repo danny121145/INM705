7cd58fa3d0f45e7a16b6cc94631aebaed9d1647c
danny121145 <98962440+danny121145@users.noreply.github.com>
2025-04-26 23:54:12 +0100

    steup.sh mistake fixed

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 python3 -m venv venv
-source venv/bin/activate
+venv/Scripts/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-echo "Environment ready! Run 'source venv/bin/activate' to activate."
